68df5c694a43967c9a99601f326ee683ae05e356
Zunairzz <zunairsarwar1@gmail.com>
2024-10-20 15:36:21 +0500

    add my service component in home page

---
+++ frontend/src/data/ServiceData.js
@@ -0,0 +1,9 @@
+import service1 from '../assests/s1.png';
+import service2 from '../assests/s2.png';
+import service3 from '../assests/s3.png';
+
+export const serviceData = [
+    {heading: 'UI/UX Design', image: service1},
+    {heading: 'Web Design', image: service2},
+    {heading: 'Java Developer', image: service3},
+]
--- frontend/src/pages/Service.js
+++ frontend/src/pages/Service.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import Base from "../component/Base";
+import {Container} from "reactstrap";
+import my_services from '../assests/my_services.jpeg';
 
 
 const Service = () => {
     return (
         <Base>
-            <section className="services">
-                <h2>My Quality Services</h2>
-                <div className="service-list">
-                    <div className="service-item">
-                        <h3>Branding Design</h3>
-                        <p>I break down complex user experience problems to create integrity-focused solutions that
-                            connect
-                            billions of people.</p>
-                    </div>
-                    <div className="service-item">
-                        <h3>Web Design</h3>
-                        <p>I break down complex user experience problems to create integrity-focused solutions that
-                            connect
-                            billions of people.</p>
-                    </div>
-                    <div className="service-item">
-                        <h3>UI/UX Design</h3>
-                        <p>I break down complex user experience problems to create integrity-focused solutions that
-                            connect
-                            billions of people.</p>
-                    </div>
-                    <div className="service-item">
-                        <h3>Graphics Design</h3>
-                        <p>I break down complex user experience problems to create integrity-focused solutions that
-                            connect
-                            billions of people.</p>
-                    </div>
-                </div>
-            </section>
+            <div
+                className=" items-center justify-center py-10"
+                style={{
+                    backgroundImage: 'url(../assests/my_services.jpeg)',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '400px', // Adjust height as needed
+                }}>
+
+            </div>
         </Base>
     )
 }
